3a89961384da635890e819df37ceb81861d9743f
Weerayut Teja <teja.weerayut@gmail.com>
2019-05-15 17:55:39 +0700

    Chaning environment variables

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const dotenv = require('dotenv').config()
+require('dotenv').config()
 const { EventEmitter } = require('events')
 const mediator = new EventEmitter()
 const config = require('./config')
